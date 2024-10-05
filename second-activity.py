b3853d71b47c335133defb07c96cb69af52e4982
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-10-05 11:10:55 -0400

    Add files via upload

---
+++ second-activity.py
@@ -0,0 +1,13 @@
+my_dict = {}
+
+my_dict =  {1: 'apple', 2:'ball'}
+
+my_dict = {'name' : 'John' ,  1:[2,4,3]}
+
+my_dict = {'name' : 'Jack' , 'age' : 26}
+
+print(my_dict['name'])
+print(my_dict.get('age'))
+
+my_dict.pop('age')
+print(my_dict)
